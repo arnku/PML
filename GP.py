6c9a25ec6d8ccee43832bd91973ff0569604fd7b
AsgerRN <asger@snabeladk.dk>
2025-12-12 16:57:06 +0100

    Fixed everything

--- GP.py
+++ GP.py
@@ -37,51 +37,33 @@ def model():
     return samples
 
 nuts_kernel = pyro.infer.NUTS(model)
-mcmc1 = pyro.infer.MCMC(nuts_kernel, num_samples=100, warmup_steps=50)
-mcmc2 = pyro.infer.MCMC(nuts_kernel, num_samples=100, warmup_steps=50)
-mcmc1.run()
-mcmc2.run()
-samples1 = mcmc1.get_samples()['x']
-samples2 = mcmc2.get_samples()['x']
+mcmc = pyro.infer.MCMC(nuts_kernel, num_samples=100, warmup_steps=50, num_chains=2)
+mcmc.run()
+samples = mcmc.get_samples()['x']
 #Calculate E[x^2]
-expectation_x2_1 = torch.mean(samples1**2)
-data1 = arviz.from_pyro(mcmc1)
-expectation_x2_2 = torch.mean(samples2**2)
-data2 = arviz.from_pyro(mcmc2)
-summary1 = arviz.summary(data1)
-summary2 = arviz.summary(data2)
-print("Chain 1 E[x^2]:", expectation_x2_1)
-print("Chain 2 E[x^2]:", expectation_x2_2)
-print("Chain 1 Summary:\n", summary1)
-print("Chain 2 Summary:\n", summary2)
-def HMC(n_samples):
-    expected_val1_tensor = torch.tensor([])
-    expected_val2_tensor = torch.tensor([])
-    for i in range(5):
+expectation_x2 = torch.mean(samples**2)
+data = arviz.from_pyro(mcmc)
+summary = arviz.summary(data)
+print("Chain E[x^2]:", expectation_x2)
+print("Chain Summary:\n", summary)
+
+def HMC(n_samples: int) -> torch.Tensor:
+    expected_val_tensor = torch.tensor([])
+    for i in range(2):
         nuts_kernel = pyro.infer.NUTS(model)
-        mcmc1 = pyro.infer.MCMC(nuts_kernel, num_samples=n_samples, warmup_steps=200)
-        mcmc2 = pyro.infer.MCMC(nuts_kernel, num_samples=n_samples, warmup_steps=200)
-        mcmc1.run()
-        mcmc2.run()
-        samples1 = mcmc1.get_samples()["x"].detach().cpu()
-        samples2 = mcmc2.get_samples()["x"].detach().cpu()
-        expected_val1 = torch.mean(samples1**2)
-        expected_val2 = torch.mean(samples2**2)
-        expected_val1_tensor = torch.cat((expected_val1_tensor, expected_val1.unsqueeze(0)), dim=0)
-        expected_val2_tensor = torch.cat((expected_val2_tensor, expected_val2.unsqueeze(0)), dim=0)
-    return [expected_val1_tensor, expected_val2_tensor]
+        mcmc = pyro.infer.MCMC(nuts_kernel, num_samples=n_samples, warmup_steps=200, num_chains=2)
+        mcmc.run()
+        samples = mcmc.get_samples()["x"].detach().cpu()
+        expected_val = torch.mean(samples**2)
+        expected_val_tensor = torch.cat((expected_val_tensor, expected_val.unsqueeze(0)), dim=0)
+    return expected_val_tensor
+
 n_samples_list = [10,100,1000]
-expected_val_list_1 = torch.tensor([])
-expected_val_list_2 = torch.tensor([])
+expected_val_list = torch.tensor([])
 for n_samples in n_samples_list:
     expected_vals = HMC(n_samples)
-    expected_val_list_1 = torch.cat((expected_val_list_1, expected_vals[0]), dim=0)
-    expected_val_list_2 = torch.cat((expected_val_list_2, expected_vals[1]), dim=0)
-std1 = torch.std(expected_val_list_1,dim=0)
-mean1 = torch.mean(expected_val_list_1,dim=0)
-print("Mean of E[x^2]:", mean1)
-print("Standard Deviation of E[x^2]:", std1)
-std2 = torch.std(expected_val_list_2,dim=0)
-mean2 = torch.mean(expected_val_list_2,dim=0)
-print("Mean of E[x^2]:", mean2)
-print("Standard Deviation of E[x^2]:", std2)
+    expected_val_list = torch.cat((expected_val_list, expected_vals), dim=0)
+std = torch.std(expected_val_list,dim=0)
+mean = torch.mean(expected_val_list,dim=0)
+print("Mean of E[x^2]:", mean.item())
+print("Standard Deviation of E[x^2]:", std.item())
